2040836c5f50e0ac891af2d0a5f32e1c714557e3
meixg <meixg@foxmail.com>
2022-05-09 20:12:23 +0800

    init

---
+++ src/cases/forin-vs-forof.js
@@ -0,0 +1,18 @@
+const {createSuite} = require('../utils/benchmarkify');
+
+const suite = createSuite('for...in vs for...of');
+const arr = new Array(100).fill(0);
+
+suite.add('for...in', () => {
+    for (let i = 0; i < arr.length; i++) {
+        const a = arr[i];
+    }
+});
+
+suite.add('for...of', () => {
+    for (let a of arr) {
+
+    }
+});
+
+suite.run();
