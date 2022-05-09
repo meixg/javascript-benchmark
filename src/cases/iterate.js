efaeb232c33d5ecaf671938e76ba39807798c6c8
meixg <meixg@foxmail.com>
2022-05-09 20:51:23 +0800

    add

--- src/cases/iterate.js
+++ src/cases/iterate.js
@@ -3,7 +3,7 @@ const {createSuite, runSuite} = require('../utils/benchmarkify');
 const suite = createSuite('iterate', {minSamples: 1000000});
 const arr = new Array(100).fill(0);
 
-suite.add('for...in', () => {
+suite.add('for', () => {
     for (let i = 0; i < arr.length; i++) {
         const a = arr[i];
     }
